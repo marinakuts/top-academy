c990da44e1016c02ce60452ea8e592caeadd29e6
marinakuts <117716156+marinakuts@users.noreply.github.com>
2024-04-15 23:49:05 +0300

    Функции замыкания

---
+++ Kochur M HW8/script.js
@@ -0,0 +1,51 @@
+//1----------------------------
+
+// let hello = 'Hello';
+// let user = function() {
+//     let name = 'John';
+//     let sayHello = function() {
+
+//       console.log(`${hello}, ${name}`);
+//     }
+//     sayHello()
+//     return sayHello
+// }
+// user()
+
+
+//2------------------------------
+
+// let a = 1;
+// function first() {
+//   let b = 2;
+//   function second() {
+//     let c = 3;
+
+//     let summ = a + b + c;
+//     console.log(`a + b + c = ${summ}`);
+//   }
+//   second()
+//   return second
+// }
+
+// first()
+
+
+//3--------------------------
+
+// let a = 1;
+
+// function first() {
+//   let b = 2;
+//   function second() {
+//     let c = 3;
+//     let summ = a + b + c;
+//     console.log(`a + b + c = ${summ}`);
+//     a++;
+//   }
+//   second()
+//   return second
+// }
+// first()
+// first()
+// first()
